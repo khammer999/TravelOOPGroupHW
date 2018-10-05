ccbb168377d08b7e45023790408b6bcbb30e3eff
khammer999 <khammer999@gmail.com>
2018-10-05 12:59:36 -0400

    initial commit

---
+++ TravelOOP.Tests/TravelTests.cs
@@ -0,0 +1,193 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelOOP.Tests
+{
+    //Maggie, Tim, Ken Homework due 10/8/2018
+    [TestFixture]
+    public class TravelTests
+    {
+        [Test]
+        public void Test_Default_CTR()
+        {
+            //arrange
+            Travel traveller = new Travel("Harry");
+            //action
+            string response = traveller.FName;
+            //assert
+            Assert.That(response, Is.EqualTo("Harry"));
+        }
+
+        [Test]
+        public void Test_LastName_Prop()
+        {
+            //arrange
+            Travel traveller = new Travel("Harry Buffalo");
+            //action
+            string response = traveller.LName;
+            //assert
+            Assert.That(response, Is.EqualTo("Buffalo"));
+        }
+
+        [Test]
+        public void Test_Address_Prop()
+        {
+            //arrange
+            Travel traveller = new Travel("Harry Buffalo");
+            //action
+            traveller.Address=" ";
+            string response = traveller.Address;
+            //assert
+            Assert.That(response, Is.EqualTo(" "));
+        }
+
+        [Test]
+        public void Test_Address_Email()
+        {
+            //arrange
+            Travel traveller = new Travel("Harry Buffalo");
+            //action
+            traveller.Email = " ";
+            string response = traveller.Email;
+            //assert
+            Assert.That(response, Is.EqualTo(" "));
+        }
+
+        [Test]
+        public void Test_Date_Travel_Method()
+        {
+            //arrange
+            Travel traveller = new Travel();
+            //action
+            var response = traveller.GetDate(9, 8, 2018);
+            //assert
+            Assert.That(response, Is.EqualTo("9/8/2018"));
+        }
+
+        [Test]
+        public void Test_Total_Travel_Time_Method()
+        {
+            //arrange
+            Travel traveller = new Travel();
+            //action
+            var response = traveller.TtlTravel(9,13,2018);
+            //assert
+            Assert.That(response, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Selection_Method()
+        {
+            //arrange
+            Travel traveller = new Travel();
+            //action
+            var response = traveller.GetSelection(5);
+            //assert
+            Assert.That(response, Is.EqualTo("Cali"));
+        }
+
+        [Test]
+        public void Selection_Method_4_Georgia()
+        {
+            //arrange
+            Travel traveller = new Travel();
+            //action
+            var response = traveller.GetSelection(4);
+            //assert
+            Assert.That(response, Is.EqualTo("Georgia"));
+        }
+
+        [Test]
+        public void Selection_Method_3_Florida()
+        {
+            //arrange
+            Travel traveller = new Travel();
+            //action
+            var response = traveller.GetSelection(3);
+            //assert
+            Assert.That(response, Is.EqualTo("Florida"));
+        }
+
+        [Test]
+        public void Selection_Method_2_Nashville()
+        {
+            //arrange
+            Travel traveller = new Travel();
+            //action
+            var response = traveller.GetSelection(2);
+            //assert
+            Assert.That(response, Is.EqualTo("Nashville"));
+        }
+
+        [Test]
+        public void Selection_Method_1_Put_in_Bay()
+        {
+            //arrange
+            Travel traveller = new Travel();
+            //action
+            var response = traveller.GetSelection(1);
+            //assert
+            Assert.That(response, Is.EqualTo("Put in Bay"));
+        }
+
+        [Test]
+        public void Selection_Method_0_Stay_Home()
+        {
+            //arrange
+            Travel traveller = new Travel();
+            //action
+            var response = traveller.GetSelection(0);
+            //assert
+            Assert.That(response, Is.EqualTo("Stay Home"));
+        }
+
+        [Test]
+        public void Selection_Method_Greater_Than_5()
+        {
+            //arrange
+            Travel traveller = new Travel();
+            //action
+            var response = traveller.GetSelection(6);
+            //assert
+            Assert.That(response, Is.EqualTo("The World is Yours"));
+        }
+
+        [Test]
+        public void Selection_Method_Greater_Than_5_Destination()
+        {
+            //arrange
+            Destination destination = new Destination();
+            //action
+            var response = destination.GetSelection(6);
+            //assert
+            Assert.That(response, Is.EqualTo("The World is Yours"));
+        }
+
+        [Test]
+        public void Selection_Method_Greater_Than_5_Destination_TTT_Method_Input()
+        { 
+            //arrange
+            Destination destination = new Destination();
+            //action
+            var response = destination.GetSelection(destination.TtlTravel(9,14,2018));
+            //assert
+            Assert.That(response, Is.EqualTo("The World is Yours"));
+
+        }
+
+        [Test]
+        public void Selection_Method_5_Destination_TTT_Method_Input()
+        {
+            //arrange
+            Destination destination = new Destination();
+            //action
+            var response = destination.GetSelection(destination.TtlTravel(9, 13, 2018));
+            //assert
+            Assert.That(response, Is.EqualTo("Cali"));
+        }
+    }
+}
---
+++ TravelOOP/Travel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace TravelOOP
+{
+    //Maggie, Tim, Ken Homework due 10/8/2018
+    public class Travel
+    {
+        //properties
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+        //constructors
+        public Travel(string name )
+        {   
+            //splits input into first and last name
+            var words = name.Split(' ');
+            if(words.Length == 1)
+            {
+                FName = words[0];
+            }
+            else
+            {
+                FName = words[0];
+                LName = words[1];
+            }        
+        }
+        public Travel()
+        {
+            //default constructor
+        }
+        //accepts input of ints and converts into string "mm/dd/yyyy" date format
+        public string GetDate(int month, int day, int year)
+        {
+            DateTime date = new DateTime(year, month, day);           
+            string value = (month.ToString() + "/" + day.ToString() + "/" + year.ToString());
+            return value;
+        }
+
+        public int TtlTravel(int month, int day, int year)
+        {
+            //puts month, day, and year into array
+            var endDateArray = new int[] { month, day, year };
+            //converts start day from string to ints 
+            var startDate = "9/8/2018";
+            var temps = startDate.Split('/');
+            int[] startDateArray = new int[3];
+            for (int i = 0; i < temps.Length; i++)
+            {
+                startDateArray[i] = int.Parse(temps[i]);
+            }
+            //calculates total days when month is the same
+            int totalDays = 0;
+            if (endDateArray[0] == startDateArray[0])
+            {
+                totalDays = endDateArray[1] - startDateArray[1];
+            }
+            //calculate total days when year is the same
+            if(endDateArray[2] == startDateArray[2])
+            {
+                totalDays = endDateArray[1] - startDateArray[1];
+            }
+            //returns total days
+            return totalDays;
+        }
+
+        public string GetSelection(int input)
+        {
+            if (input == 0)
+            {
+                return "Stay Home";
+            }
+            else if (input == 1)
+            {
+                return "Put in Bay";
+            }
+            else if (input == 2)
+            {
+                return "Nashville";
+            }
+            else if (input == 3)
+            {
+                return "Florida";
+            }
+            else if (input == 4)
+            {
+                return "Georgia";
+            }
+            else if(input == 5)
+            {
+                return "Cali";
+            }else{
+                return "The World is Yours";
+            }
+        }
+    }
+}
